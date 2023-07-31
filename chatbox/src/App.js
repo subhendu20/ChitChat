ce5c7800cafe5670a5615ab91e0c61b75cbc3c9f
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-07-31 10:02:37 +0530

    new

--- chatbox/src/App.js
+++ chatbox/src/App.js
@@ -342,6 +342,12 @@ function App() {
 //useEfect functions
   }
 
+  useEffect(()=>{
+    let vh = window.innerHeight * 0.01;
+  
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  },[])
+
   useEffect(() => {
     logcheck()
   })
--- chatbox/src/Components/Chatbox.js
+++ chatbox/src/Components/Chatbox.js
@@ -25,7 +25,7 @@ function Chatbox({ room, name, account }) {
 
   useEffect(() => {
     scrollToBottom()
-  }, [room,relaod]);
+  }, [room,relaod,filterchat]);
 
 
 
